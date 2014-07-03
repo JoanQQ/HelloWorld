b4299dc9838cb152c17e48c6f73deaf138d09534
Phyllis Zhao <phyllis@happylatte.com>
2014-07-03 14:31:39 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Phyllis Zhao on 7/3/14.
+//  Copyright (c) 2014 Happylatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
