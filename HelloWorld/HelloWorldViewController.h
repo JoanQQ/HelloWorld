bcb19fe5b88d27a821c47d613d9dde0cc72dfe4c
Phyllis Zhao <phyllis@happylatte.com>
2014-07-03 15:27:36 +0800

    connect local project to github

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Phyllis Zhao on 7/3/14.
+//  Copyright (c) 2014 Happylatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,47 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Phyllis Zhao on 7/3/14.
+//  Copyright (c) 2014 Happylatte. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showMessage
+{
+    UIAlertView *helloWorldAlert = [[UIAlertView alloc]
+                                    initWithTitle:@"My First App" message:@"Hello, World!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    // Display the Hello World Message
+    [helloWorldAlert show];
+}
+
+@end
